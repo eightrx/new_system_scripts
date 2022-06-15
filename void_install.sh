1223ff1ee9b34cacb4c83da0d18b666c38affac2
Gavin <97205920+eightrx@users.noreply.github.com>
2022-06-15 12:29:15 -0700

    adding packages and rofi

--- void_install.sh
+++ void_install.sh
@@ -2,9 +2,13 @@
 
 echo Installing packages
   sudo xbps-insatll -Su xbps
-  sudo xbps-install -Syu zsh cargo ranger git curl kitty helix btop lightdm lightdm-gtk3-greeter base-devel cairo-devel clthreads-devel libxcb-devel cmake gdb ninja xcb-proto xcb-util-devel xcb-util-cursor-devel xcb-util-keysyms-devel xcb-util-wm-devel gtkmm-devel
+  sudo xbps-install -Syu rofi zsh cargo ranger git curl kitty helix btop lightdm lightdm-gtk3-greeter base-devel cairo-devel clthreads-devel libxcb-devel cmake gdb ninja xcb-proto xcb-util-devel xcb-util-cursor-devel xcb-util-keysyms-devel xcb-util-wm-devel gtkmm-devel
 
 echo Moving around config files
+  mkdir ~/.config/rofi/
+  cd ~/source
+  git clone https://github.com/yuky2020/rofi-themes.git
+  cp -r rofi-themes/* ~/.config/rofi/
   cp .zshrc ~/.zshrc
   cp helix_config.toml ~/.config/helix/config.toml
   cp hypr.conf ~/.config/hypr/hypr.conf
