76e938289da30526ed13da2ea602f22ce8726335
Gavin <97205920+eightrx@users.noreply.github.com>
2022-06-19 16:11:45 -0700

    Update void_install.sh

--- void_install.sh
+++ void_install.sh
@@ -4,7 +4,7 @@ echo
 echo Installing packages
 echo
 sudo xbps-install -Syu xbps
-sudo xbps-install -Syu void-repo-nonfree xfce4 zsh cargo ranger git curl kitty picom helix btop lightdm lightdm-gtk3-greeter base-devel cairo-devel clthreads-devel libxcb-devel cmake gdb ninja xcb-proto xcb-util-devel xcb-util-cursor-devel xcb-util-keysyms-devel xcb-util-wm-devel gtkmm-devel
+sudo xbps-install -Syu void-repo-nonfree xfce4 zsh cargo rofi ranger git curl kitty picom helix btop lightdm lightdm-gtk3-greeter base-devel cairo-devel clthreads-devel libxcb-devel cmake gdb ninja xcb-proto xcb-util-devel xcb-util-cursor-devel xcb-util-keysyms-devel xcb-util-wm-devel gtkmm-devel
 sudo xbps-remove -y xfdesktop
 
 echo
@@ -12,13 +12,15 @@ echo Moving around config files
 echo
   mkdir ~/.config/helix/
   cp helix_config.toml /home/gavin/.config/helix/config.toml
-  mkdir ~/.config/hypr
+  mkdir ~/.config/hypr/
   cp hypr.conf /home/gavin/.config/hypr/hypr.conf
   sudo mkdir /usr/share/xsessions/
   sudo cp hypr.desktop /usr/share/xsessions/hypr.desktop
   sudo mkdir /usr/lib/helix/runtime/themes/
   sudo cp onedark.toml /usr/lib/helix/runtime/themes/
   sudo cp helperScript /usr/share/helperScript
+  mkdir ~/.config/rofi/default/
+  cp config.rasi ~/.config/rofi/default/config.rasi
 
 echo
 echo Cloning and compiling HyprWM
