3354d497594f3b758a8f4e5786b3c3596dd2a449
Gavin <97205920+eightrx@users.noreply.github.com>
2022-07-22 13:46:50 -0700

    Create themes.sh

---
+++ themes.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+mkdir themes
+cd themes
+git clone https://github.com/vinceliuice/WhiteSur-gtk-theme.git
+wget https://ppload-com.s3.eu-central-1.amazonaws.com/data/files/1545654101/Breeze-Chameleon-Dark.tar.gz?response-content-disposition=attachment%3B%2520Breeze-Chameleon-Dark.tar.gz&X-Amz-Content-Sha256=UNSIGNED-PAYLOAD&X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIATLQUPBWASZL2ZPWI%2F20220722%2Feu-central-1%2Fs3%2Faws4_request&X-Amz-Date=20220722T204604Z&X-Amz-SignedHeaders=host&X-Amz-Expires=60&X-Amz-Signature=71d55937163d5f457b5ce8375a344a1524a8adf76fecdd5a7ae98057bb89637f
